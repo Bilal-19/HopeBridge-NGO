c107bf5f5edb3eafaca3b6d59df3569377e4f93a
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-01-20 16:09:09 +0500

    Updated All Users page.

--- resources/views/Admin/FAQ.blade.php
+++ resources/views/Admin/FAQ.blade.php
@@ -1,8 +1,8 @@
 @extends('adminDashboard')
 @section('main-section')
     <div class="container-fluid">
-        <div class="row mt-5">
-            <h3>Freqeuntly Asked <span class="text-dark-green fw-bold">Questions</span></h3>
+        <div class="row mt-3">
+            <h3 class="text-center">Freqeuntly Asked <span class="text-dark-green fw-bold">Questions</span></h3>
         </div>
 
         <div class="row mt-3">
--- resources/views/Admin/Users.blade.php
+++ resources/views/Admin/Users.blade.php
@@ -1,8 +1,8 @@
 @extends('adminDashboard')
 @section('main-section')
     <div class="container-fluid">
-        <div class="row">
-            <h1>Users</h1>
+        <div class="row mt-3">
+            <h3 class="text-center">All <span class="text-dark-green fw-bold">Users</span></h3>
         </div>
     </div>
 @endsection
